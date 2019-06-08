333de59f770c644cc928ae202d84f2ba887fd924
Gabriel Lima <gabriel.luz.lima@hotmail.com>
2019-06-08 19:23:33 -0300

    crawler 1.1

--- media-de-temperatura/crawler.py
+++ media-de-temperatura/crawler.py
@@ -1,4 +1,16 @@
+import csv
 import requests
+from bs4 import BeautifulSoup as bs
+
+
 r = requests.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/558/saopaulo-sp')
 texto = r.text
-print(r)
+# print(texto)
+
+
+def acha_temperatura():
+    soup = bs(texto, 'html.parser')
+    temp_max = soup.find('p', id='tempMax0').text
+    temp_min = soup.find('p', id='tempMin0').text
+    print(temp_max)
+    print(temp_min)
