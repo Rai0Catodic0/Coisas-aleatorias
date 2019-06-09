54eab850af7deda8e72f17a46c4822dafcb68864
Gabriel Lima <gabriel.luz.lima@hotmail.com>
2019-06-09 12:42:55 -0300

    tabela com as temperaturas e crawler adicionando as temperaturas no csv

--- media-de-temperatura/crawler.py
+++ media-de-temperatura/crawler.py
@@ -1,16 +1,32 @@
-import csv
+# encontra a temperatura e salva em um csv
 import requests
 from bs4 import BeautifulSoup as bs
+import time
 
-
+# criando arquivo csv pra armazenar os dados da tabela
+tabela = open('temperaturas.csv', 'w')
+tabela.write('dia;temperatura maxima; temperatura minima \n')
+# requisição do html do clima tempo
 r = requests.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/558/saopaulo-sp')
 texto = r.text
-# print(texto)
+
+# função pra encontrar as temperaturas maximas e minimas do dia
 
 
 def acha_temperatura():
     soup = bs(texto, 'html.parser')
     temp_max = soup.find('p', id='tempMax0').text
     temp_min = soup.find('p', id='tempMin0').text
-    print(temp_max)
-    print(temp_min)
+    return temp_max, temp_min
+
+
+# função pra salvar a temperatura maxima ,minima e dia no csv
+
+
+def salva_temp(temp_min, temp_max):
+    data = time.localtime()  # data do dia
+    tabela.write(str(data[2])+';'+temp_min+';'+temp_max+'\n')
+
+
+temp = acha_temperatura()
+salva_temp(temp[0], temp[1])
